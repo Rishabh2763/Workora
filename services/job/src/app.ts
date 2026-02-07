fb846df1e663629148c523eb5af71bf086bcd45d
Rishabh <rishabhlakhaiyar27@gmail.com>
2026-02-07 12:18:59 +0530

    Add job service with company, job, and application management

---
+++ services/job/src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import jobRoutes from "./routes/job.js";
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/job',jobRoutes);
+export default app;
